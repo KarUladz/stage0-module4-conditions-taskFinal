26f85a9bd9b039f47176141562443997fcfc2033
KarUladz <u.karnialiuk@gmail.com>
2025-07-06 20:40:27 +0300

    feat: add m4-finalTask

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,43 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if (year >= 0) {
+            boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+            int februaryDaysCount = isLeapYear ? 29 : 28;
+            printCountDaysMessage(februaryDaysCount, month);
+        } else {
+            printInvalidMessage();
+        }
     }
+
+    public static void printCountDaysMessage(int februaryDaysCount, int month) {
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("31");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("30");
+                break;
+            case 2:
+                System.out.println(februaryDaysCount);
+                break;
+            default:
+                printInvalidMessage();
+                break;
+        }
+    }
+
+    public static void printInvalidMessage() {
+        System.out.println("invalid date");
+    }
+
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        if (divider == 0) {
+            System.out.println("division by zero");
+            return;
+        }
+
+        int divisionResult = dividend / divider;
+        int multiplyResult = divisionResult * divider;
+
+        if(multiplyResult == dividend) {
+            System.out.println("can be divided completely");
+        } else {
+            System.out.println("cannot be divided completely");
+        }
+
     }
 }
